044102705e92f5cba3126e7ed0f08bf5751ae300
Biozid Bhuiyan Tonoy <biozidbhuiyantonoy@gmail.com>
2026-05-18 14:17:01 +0600

    add Navbar Component and main hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
+import Navbar from "../components/Navbar";
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <Navbar/>
+    <main>
+      <section className="hero flex flex-col justify-center items-center h-screen">
+        <h1 className="text-4xl font-bold">Welcome to the Shopping cart</h1>
+        <p className="text-2xl">Here you will get the best product</p>
+      </section>
+    </main>
+    </>
   )
 }
 
-export default Home
+export default Home;
