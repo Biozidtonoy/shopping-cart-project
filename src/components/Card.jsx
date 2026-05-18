d9e6f6276a0dc6a5c59f89df0fe3d01e2f8aced2
Biozid Bhuiyan Tonoy <biozidbhuiyantonoy@gmail.com>
2026-05-18 14:13:45 +0600

    update the card component so that i can use it shop page

---
+++ src/components/Card.jsx
@@ -0,0 +1,26 @@
+import '../styles/card.css'
+import { Link } from 'react-router';
+
+function Card({id, title , price, imgUrl}) {
+
+  return (
+    <>
+      <div className="card">
+        <div className="productImg"><img src={imgUrl} alt={title} /></div>
+        <div className="productTitle">
+          <h1>{title} </h1>
+        </div>
+        <div className="productPrice">
+          <p>Price : {price}</p>
+        </div>
+        <div className="detailsBtn">
+          <Link to={`/product/${id}`}>
+          <button>See product details</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Card
